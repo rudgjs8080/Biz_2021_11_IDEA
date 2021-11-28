deafb3510c0fb8b6f903f6dd2939e115bc7c0694
rudgjs8080 <kkh_8080@naver.com>
2021-11-29 00:59:48 +0900

    1128 home MEETT security Login Google complete

---
+++ SpringBoot_test_gradle_v1/src/main/java/com/rudgjs8080/book/domain/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.rudgjs8080.book.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
