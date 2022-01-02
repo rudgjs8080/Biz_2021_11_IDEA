5ef04c8b7b16178ef0ba962314e3a85c08b62718
rudgjs8080 <kkh_8080@naver.com>
2022-01-03 01:07:32 +0900

    0102 fullcalendar 사용법

---
+++ SpringBoot_Calendar_v1/src/test/java/com/rudgjs8080/calendar/CalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudgjs8080.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ study/src/main/java/com/rudgjs8080/study/domain/OrderSearch.java
@@ -0,0 +1,7 @@
+package com.rudgjs8080.study.domain;
+
+public class OrderSearch {
+
+    private String memberName; // 회원 이름
+    private OrderStatus orderStatus; // 주문상태
+}
